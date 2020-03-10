85ce8e1647c59576a9f3fb5dabf552a982fd279c
1349621303 <1349621303@qq.com>
2020-03-10 15:41:10 +0800

    Project PetstorePro push to GitHub.

---
+++ src/main/java/com/csu/petstorepro/PetstoreProApplication.java
@@ -0,0 +1,13 @@
+package com.csu.petstorepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstoreProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetstoreProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csu/petstorepro/PetstoreProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csu.petstorepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetstoreProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
